9707ac705578e22ef343ca5071748e69e8c9b6d9
Simon Fong <simonfong6@gmail.com>
2017-11-30 18:35:48 -0800

    Added next_batch to Dataset class and only load in batches

--- simon/data.py
+++ simon/data.py
@@ -2,27 +2,31 @@ import os
 import cv2
 import numpy as np
 import json
+from operator import itemgetter
 
-class Data:
+class Dataset:
     def __init__(self,data_dir,height,width):
         self.data_dir = data_dir
         self.height = height
         self.width = width
         self.num_classes = 0
+        self.image_count = 0
+        self.image_paths = []
         self.images = []
         self.names = {}
         self.labels = []
         
-    def load_data(self):
-        image_count = 0
+    def read_data(self):
+        """Creates a list of image paths and labels"""
         dir_names = os.listdir(self.data_dir)
         dir_names.sort()
         num_classes = len(dir_names)
-        for dir_name in dir_names:
+        
+        for index,dir_name in enumerate(dir_names):
             
             # Create a mapping from label number to name
-            label = int(dir_name[:3])-1
-            name = dir_name[3:]
+            label = index
+            name = dir_name
             self.names[label] = name
             
             # Get images directory path
@@ -31,11 +35,9 @@ class Data:
             image_names.sort()
             
             for image_name in image_names:
-                # Read, resize, and store image
+                # Read, resize, and store image path
                 image_path = os.path.join(dir_path, image_name)
-                image = cv2.imread(image_path)
-                image_resized = cv2.resize(image, (self.height,self.width))
-                self.images.append(image_resized)
+                self.image_paths.append(image_path)
                 
                 # Add image label array
                 label_array = np.zeros((1,num_classes))
@@ -43,16 +45,62 @@ class Data:
                 self.labels.append(label_array)
                 # print "loaded {}".format(image_name)
                 
-                image_count += 1
+                self.image_count += 1
         
         print("Loaded data from {}".format(self.data_dir))
+        
+    def load_data(self, image_paths):
+        """Loads images as numpy arrays and returns them."""
+        
+        # Stores image arrays
+        images = []
+        
+        # Read images and store in images list
+        for image_path in image_paths:
+            image = cv2.imread(image_path)
+            image_resized = cv2.resize(image, (self.height,self.width))
+            images.append(image_resized)
+        
+        return images
+        
+        
+    def next_batch(self,batch_size):
+        """Return the next `batch_size` examples from this data set."""
+        
+        # Create a random seed
+        perm = np.arange(self.image_count)
+        np.random.shuffle(perm)
+        
+        # Shuffle paths and get a batch of image arrays
+        image_paths = [ self.image_paths[i] for i in perm ]
+        image_paths = image_paths[:batch_size]
+        batch_images = self.load_data(image_paths)
+        
+        # Shuffle labels and get a batch
+        batch_labels = [ self.labels[i] for i in perm ]
+        batch_labels = batch_labels[:batch_size]
+        
+        return batch_images, batch_labels
     
                 
         
 def main():
-    data = Data('256_ObjectCategories', 299, 299)
-    data.load_data()
-    print json.dumps(data.names, indent=4, sort_keys=True)
+    
+    data = Dataset('train', 299, 299)
+    data.read_data()
+    
+    #print json.dumps(data.names, indent=4, sort_keys=True)
+    
+    names = data.names
+    
+    images, labels = data.next_batch(50)
+    
+    for image,label in zip(images,labels):
+        name = names[np.argmax(label)]
+        print("{} \t {}".format(name,label))
+        cv2.imshow(name,image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
     
 if __name__ == '__main__':
     main()
