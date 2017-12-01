bdab8fc19dd8064399e8f1df6ed67578860404da
Simon Fong <simonfong6@gmail.com>
2017-11-30 20:47:34 -0800

    Fixed y_conv being init and not graph

--- simon/main.py
+++ simon/main.py
@@ -7,8 +7,8 @@ import numpy as np
 # Constants
 IMAGE_WIDTH = 299
 IMAGE_HEIGHT = 299
-EPOCHS = 100
-BATCH_SIZE = 50
+EPOCHS = 10
+BATCH_SIZE = 10
 
 # Load dataset
 ndsb = Dataset('train',IMAGE_HEIGHT,IMAGE_WIDTH)
@@ -16,18 +16,17 @@ ndsb.read_data()
 num_classes = ndsb.num_classes
 
 # Placeholder inputs and output
-inputs = tf.placeholder("float", [None,IMAGE_HEIGHT,IMAGE_WIDTH,1])
-predict = tf.placeholder("float", [None,1,1,num_classes])
+inputs = tf.placeholder(tf.float32, [None,IMAGE_HEIGHT,IMAGE_WIDTH,3])
+predict = tf.placeholder(tf.float32, [None,1,1,num_classes])
 
 # Get model
-base, some = inception_v3(inputs, num_classes=num_classes)
+y_conv, some = inception_v3(inputs, num_classes=num_classes)
 
 # Init tf variables
-y_conv = tf.global_variables_initializer()
+init = tf.global_variables_initializer()
 
 # Cross entropy graph
-cross_entropy = tf.reduce_mean(
-      tf.nn.softmax_cross_entropy_with_logits(labels=predict, logits=y_conv))
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=predict, logits=y_conv))
 
 # Training graph
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -43,11 +42,11 @@ with tf.Session() as sess:
     
     for i in range(EPOCHS):
         images, labels = ndsb.next_batch(BATCH_SIZE)
-        
+        '''
         if i % 10 == 0:
             train_accuracy = accuracy.eval(feed_dict={
                 inputs: images, predict: labels})
-            print('step %d, training accuracy %g' % (i, train_accuracy))
+            print('step %d, training accuracy %g' % (i, train_accuracy))'''
         
         _, loss = sess.run([train_step, cross_entropy], 
                             feed_dict={inputs: images, predict: labels})
