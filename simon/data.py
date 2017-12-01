156903de0129d57d5512ee738d5d474f96d16c27
Simon Fong <simonfong6@gmail.com>
2017-11-30 21:46:13 -0800

    Fixed label shape issue (None,1,1,num_classes) to (None,num_classes)

--- simon/data.py
+++ simon/data.py
@@ -39,8 +39,9 @@ class Dataset:
                 self.image_paths.append(image_path)
                 
                 # Add image label array
-                label_array = np.zeros((1,1,self.num_classes))
-                label_array[0,0,label] = 1
+                # MAKE SURE THIS MATCHES WITH OTHER SIZES
+                label_array = np.zeros((self.num_classes))
+                label_array[label] = 1
                 self.labels.append(label_array)
                 # print "loaded {}".format(image_name)
                 
--- simon/main.py
+++ simon/main.py
@@ -7,8 +7,8 @@ import numpy as np
 # Constants
 IMAGE_WIDTH = 299
 IMAGE_HEIGHT = 299
-EPOCHS = 10
-BATCH_SIZE = 10
+EPOCHS = 100
+BATCH_SIZE = 50
 
 # Load dataset
 ndsb = Dataset('train',IMAGE_HEIGHT,IMAGE_WIDTH)
@@ -17,13 +17,13 @@ num_classes = ndsb.num_classes
 
 # Placeholder inputs and output
 inputs = tf.placeholder(tf.float32, [None,IMAGE_HEIGHT,IMAGE_WIDTH,3])
-predict = tf.placeholder(tf.float32, [None,1,1,num_classes])
+predict = tf.placeholder(tf.float32, [None,num_classes])
 
 # Get model
 y_conv, some = inception_v3(inputs, num_classes=num_classes)
 
-# Init tf variables
-init = tf.global_variables_initializer()
+print(y_conv.shape)
+print(predict.shape)
 
 # Cross entropy graph
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=predict, logits=y_conv))
@@ -37,16 +37,21 @@ correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(predict, 1))
 # Accuracy graph
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+# Init tf variables
+# MUST BE AFTER ALL VARIABLES
+init = tf.global_variables_initializer()
+
 with tf.Session() as sess:
     sess.run(init)
     
     for i in range(EPOCHS):
         images, labels = ndsb.next_batch(BATCH_SIZE)
-        '''
+        
         if i % 10 == 0:
+            print(labels[0].shape)
             train_accuracy = accuracy.eval(feed_dict={
                 inputs: images, predict: labels})
-            print('step %d, training accuracy %g' % (i, train_accuracy))'''
+            print('step %d, training accuracy %g' % (i, train_accuracy))
         
         _, loss = sess.run([train_step, cross_entropy], 
                             feed_dict={inputs: images, predict: labels})
