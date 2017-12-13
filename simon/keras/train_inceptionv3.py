50c680f40fc90b232ccdbb85758a328fd62531d7
Simon Fong <simonfong6@gmail.com>
2017-12-12 20:43:32 -0800

    Plots fixed and stats writing fixed

--- simon/keras/train_inceptionv3.py
+++ simon/keras/train_inceptionv3.py
@@ -1,33 +1,41 @@
 #!/env/bin/python
 """
-COGS 118B Project
-Author: Simon Fong, Thinh Le, Wilson Tran
+COGS 181
 """
+# Import native modules
+import os
+import json
+import random
+from datetime import datetime                    # Use to record time
 
+# Import Keras functions
 from keras.models import Model
 from keras.applications.inception_v3 import InceptionV3
 from keras import optimizers
 from keras.layers import Dropout, Flatten, Dense
 from keras.utils.np_utils import to_categorical
+
+# Import matrix and plotting
 import numpy as np
-import os
-import cv2
-import random
 import matplotlib
-from datetime import datetime                   # Use to record time
-import json                                     # Writing data to logger
 matplotlib.use('Agg')                           # Stops from plotting to screen
 import matplotlib.pyplot as plt
-from dataset import Dataset                     # Custom Dataset
+
+# Import OpenCV
+import cv2
+
+# Import custom dataset class
+from dataset import Dataset                     
 
 DATASET_NAME = 'plankton'
-IMAGE_WIDTH,IMAGE_HEIGHT,NUM_CHANNELS = 299,299,3
-EPOCHS = 10
+EPOCHS = 100
 BATCH_SIZE = 50
-NUM_TRAIN,NUM_VAL,NUM_TEST = 10,5,85
+NUM_TRAIN,NUM_VAL,NUM_TEST = 20,15,65
+
+IMAGE_WIDTH,IMAGE_HEIGHT,NUM_CHANNELS = 299,299,3
 
 
-ID = "{}_{}_{}_{}_{}_{}_{}".format("5De256relue",DATASET_NAME,
+ID = "{}_{}_{}_{}_{}_{}_{}".format("normal",DATASET_NAME,
                                 EPOCHS,BATCH_SIZE,NUM_TRAIN,NUM_VAL,NUM_TEST)
 
 
@@ -36,6 +44,27 @@ cal = Dataset(DATASET_NAME,IMAGE_HEIGHT,IMAGE_WIDTH)
 cal.read_data()
 cal.train_val_test_split(NUM_TRAIN,NUM_VAL,NUM_TEST)
 num_classes = cal.num_classes
+
+def logger(message):
+    """Logs any message into a file"""
+    with open('./models/stats.txt', 'a+') as f:
+        print >>f, message
+        print(message)
+
+def plot(datas, title, xlabel, ylabel, file_name):
+    """Plots the data"""
+    plt.figure()
+    for key,value in datas.iteritems():
+        plt.plot(value, label=key)
+    plt.legend(loc='upper right')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    plots_dir = 'plots'
+    file_name + '.png'
+    plot_path = os.path.join(plots_dir,file_name)    
+    plt.savefig(plot_path)
 
 def load_model():
     """Returns a pretrained model"""
@@ -50,12 +79,7 @@ def load_model():
     x = Flatten()(base_out)
     x = Dense(256,activation='relu')(x)
     x = Dropout(0.5)(x)
-    x = Dense(256,activation='relu')(x)
-    x = Dense(256,activation='relu')(x)
-    x = Dense(256,activation='relu')(x)
-    x = Dense(256,activation='relu')(x)
-    x = Dense(256,activation='relu')(x)
-
+    
     # Final fully connected layer to work with our data
     predictions = Dense(num_classes,activation='softmax')(x)
 
@@ -71,25 +95,18 @@ def load_model():
     print("Model compiled")
 
     return model
-    
-def logger(message):
-    """Logs any message into a file"""
-    with open('./models/stats.txt', 'a+') as f:
-        print >>f, message
-
 
 def main():
     
+    description = "Base Inception V3"
+    
+    logger(ID)
+    logger(description)
+    
     # Make model
     model = load_model()
-    print("Model created\n")
+    print("Model created")
 
-    # Init data array to plot
-    train_acc = np.array([])
-    train_val_acc = np.array([])
-    train_loss = np.array([])
-    train_val_loss = np.array([])
-    
     # Load the training data
     X_train, Y_train = cal.load_training()
     
@@ -98,7 +115,6 @@ def main():
     
     # Start time
     start_time = datetime.now()
-    print('Start Time', start_time)
     
     # Train model and store stats in history
     history = model.fit(x=X_train,y=Y_train,batch_size=BATCH_SIZE,
@@ -106,61 +122,44 @@ def main():
     
     # End time
     stop_time = datetime.now()
-    print('Stop Time', stop_time)
     
     # Print total time
     elapsed_time = stop_time - start_time
-    print('My Elapsed Time', elapsed_time)
+    logger("Elapsed Time")
     logger(elapsed_time)   
-
-    # Append the accuracy and loss scores
-    train_acc = np.append(train_acc, history.history['acc'])
-    train_val_acc = np.append(train_val_acc, history.history['val_acc'])
-    train_loss = np.append(train_loss, history.history['loss'])
-    train_val_loss = np.append(train_val_loss, history.history['val_loss'])
-    
      
     # Save model weights
-    model.save('./models/{}.h5'.format(ID))
-    logger(ID)
-    logger(history.history)
-    print("Model weights saved.")
-
+    model_dir = 'models'
     
-    # Plot accuracy
-    plt.hold(True)
-    plt.plot(train_acc)
-    plt.plot(train_val_acc)
-    plt.legend(loc='upper right')
-    plt.title('Model Accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.savefig('./plots/acc_vs_val_acc_{}.png'.format(ID))
-    plt.hold(False)
-    plt.show()
+    model_name = '{}'.format(ID)
+    model_name += '.h5'
+    model_path = os.path.join(model_dir,model_name)
+    
+    model.save(model_path)
 
-    # Plot loss
-    plt.hold(True)
-    plt.plot(train_loss)
-    plt.plot(train_val_loss)
-    plt.legend(loc='upper right')
-    plt.title('Model Loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.savefig('./plots/loss_vs_val_loss_{}.png'.format(ID))
-    plt.hold(False)
-    plt.show()
+    logger(history.history)
+    print("Model weights saved")
+    
+    accuracy = {'Training': history['acc'],
+                'Validation': history['val_acc']}
+                
+    loss = {'Training': history['loss'],
+            'Validation': history['val_loss']}
+    
+    # Plot training vs validation accuracy  
+    plot(accuracy, "Accuracy: Training vs Validation",
+            'Epochs', 'Accuracy', '{}_accuracy_train_val'.format(ID))
+    
+    # Plot training vs validation loss  
+    plot(loss, "Loss: Training vs Validation",
+            'Epochs', 'Loss', '{}_loss_train_val'.format(ID))
     
     # Test the model
     X_test, Y_test = cal.load_testing()
     metrics = model.evaluate(x=X_test,y=Y_test, batch_size=BATCH_SIZE)
     
-    print(metrics)
     logger(metrics)
-    print(model.metrics_names)
     logger(model.metrics_names)
-
-    return 0
 
 
 if __name__ == '__main__':
